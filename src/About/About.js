9933f49838122e8b88d1018f63e92fca351604f3
laurarugeles <laura.rugeles@aspsols.com>
2019-07-14 19:51:18 -0500

    added footer and contact info to about

--- src/About/About.js
+++ src/About/About.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
+import Footer from '../shared/Footer/Footer'
+import ContactInfo from '../shared/ContactInfo/ContactInfo'
 
 class About extends Component {
   render(){
     return (
       <div>
         About
+        <ContactInfo />
+        <Footer />
       </div>
     )
   }
